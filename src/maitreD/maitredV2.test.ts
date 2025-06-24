edfb371dc2408779d11b5e23a46504f40a2a29a5
Vishnu Ganesan <vishnukg@gmail.com>
2025-06-24 23:09:16 +1000

    Added eslint and prettier

--- src/maitreD/maitredV2.test.ts
+++ src/maitreD/maitredV2.test.ts
@@ -5,86 +5,86 @@ import { ILogger } from "../cross-cutting";
 import { IReservationRepository } from "../repository";
 
 test("CanReserve returns true when there's capacity available", () => {
-  const capacity = 10;
-  const bookedSeats = 3;
-  const stubRepository = mock<IReservationRepository>();
-  stubRepository.getReservationQuantity.mockReturnValue(bookedSeats);
-  const stubLogger = mock<ILogger>();
-  const reservation: Reservation = {
-    id: 1,
-    Date: "12/12/2022",
-    Quantity: 3,
-  };
-  const sut = new MaitreDV2(capacity, stubRepository, stubLogger);
+    const capacity = 10;
+    const bookedSeats = 3;
+    const stubRepository = mock<IReservationRepository>();
+    stubRepository.getReservationQuantity.mockReturnValue(bookedSeats);
+    const stubLogger = mock<ILogger>();
+    const reservation: Reservation = {
+        id: 1,
+        Date: "12/12/2022",
+        Quantity: 3
+    };
+    const sut = new MaitreDV2(capacity, stubRepository, stubLogger);
 
-  const result = sut.canReserve(reservation);
+    const result = sut.canReserve(reservation);
 
-  assert.equal(result, true);
+    assert.equal(result, true);
 });
 
 test("CanReserve returns false when there's no capacity available", () => {
-  const capacity = 10;
-  const bookedSeats = 10;
-  const stubRepository = mock<IReservationRepository>();
-  stubRepository.getReservationQuantity.mockReturnValue(bookedSeats);
-  const stubLogger = mock<ILogger>();
-  const reservation: Reservation = {
-    id: 1,
-    Date: "12/12/2022",
-    Quantity: 3,
-  };
-  const sut = new MaitreDV2(capacity, stubRepository, stubLogger);
+    const capacity = 10;
+    const bookedSeats = 10;
+    const stubRepository = mock<IReservationRepository>();
+    stubRepository.getReservationQuantity.mockReturnValue(bookedSeats);
+    const stubLogger = mock<ILogger>();
+    const reservation: Reservation = {
+        id: 1,
+        Date: "12/12/2022",
+        Quantity: 3
+    };
+    const sut = new MaitreDV2(capacity, stubRepository, stubLogger);
 
-  const result = sut.canReserve(reservation);
+    const result = sut.canReserve(reservation);
 
-  assert.equal(result, false);
+    assert.equal(result, false);
 });
 
 test("Get Total Capacity returns total capacity", () => {
-  const capacity = 10;
-  const stubRepository = mock<IReservationRepository>();
-  const stubLogger = mock<ILogger>();
-  const sut = new MaitreDV2(capacity, stubRepository, stubLogger);
+    const capacity = 10;
+    const stubRepository = mock<IReservationRepository>();
+    const stubLogger = mock<ILogger>();
+    const sut = new MaitreDV2(capacity, stubRepository, stubLogger);
 
-  const result = sut.getTotalCapacity();
+    const result = sut.getTotalCapacity();
 
-  assert.equal(result, capacity);
+    assert.equal(result, capacity);
 });
 
 test("CanReserve when called invokes logger with message", () => {
-  const capacity = 10;
-  const bookedSeats = 3;
-  const stubRepository = mock<IReservationRepository>();
-  stubRepository.getReservationQuantity.mockReturnValue(bookedSeats);
-  const mockLogger = mock<ILogger>();
-  const sut = new MaitreDV2(capacity, stubRepository, mockLogger);
-  const reservation: Reservation = {
-    id: 1,
-    Date: "12/12/2022",
-    Quantity: 3,
-  };
+    const capacity = 10;
+    const bookedSeats = 3;
+    const stubRepository = mock<IReservationRepository>();
+    stubRepository.getReservationQuantity.mockReturnValue(bookedSeats);
+    const mockLogger = mock<ILogger>();
+    const sut = new MaitreDV2(capacity, stubRepository, mockLogger);
+    const reservation: Reservation = {
+        id: 1,
+        Date: "12/12/2022",
+        Quantity: 3
+    };
 
-  sut.canReserve(reservation);
+    sut.canReserve(reservation);
 
-  expect(mockLogger.Log).toHaveBeenCalledWith(
-    "Checking if the reservation can be made"
-  );
+    expect(mockLogger.Log).toHaveBeenCalledWith(
+        "Checking if the reservation can be made"
+    );
 });
 
 test("CanReserve when called invokes getReservationQuantity from repository", () => {
-  const capacity = 10;
-  const mockRepository = mock<IReservationRepository>();
-  const stubLogger = mock<ILogger>();
-  const sut = new MaitreDV2(capacity, mockRepository, stubLogger);
-  const reservation: Reservation = {
-    id: 1,
-    Date: "12/12/2022",
-    Quantity: 3,
-  };
+    const capacity = 10;
+    const mockRepository = mock<IReservationRepository>();
+    const stubLogger = mock<ILogger>();
+    const sut = new MaitreDV2(capacity, mockRepository, stubLogger);
+    const reservation: Reservation = {
+        id: 1,
+        Date: "12/12/2022",
+        Quantity: 3
+    };
 
-  sut.canReserve(reservation);
+    sut.canReserve(reservation);
 
-  expect(mockRepository.getReservationQuantity).toHaveBeenCalledWith(
-    reservation.Date
-  );
+    expect(mockRepository.getReservationQuantity).toHaveBeenCalledWith(
+        reservation.Date
+    );
 });
--- src/maitreD/maitredV2.ts
+++ src/maitreD/maitredV2.ts
@@ -3,34 +3,34 @@ import { ILogger } from "../cross-cutting";
 import { IReservationRepository } from "../repository";
 
 export class MaitreDV2 implements IMaitreD {
-  private capacity: number;
-  private reservationRepo: IReservationRepository;
-  private logger: ILogger;
+    private capacity: number;
+    private reservationRepo: IReservationRepository;
+    private logger: ILogger;
 
-  constructor(
-    capacity: number,
-    reservationRepo: IReservationRepository,
-    logger: ILogger
-  ) {
-    this.capacity = capacity;
-    this.reservationRepo = reservationRepo;
-    this.logger = logger;
-  }
-
-  getTotalCapacity(): number {
-    return this.capacity;
-  }
-
-  canReserve(reservation: Reservation): boolean {
-    this.logger.Log("Checking if the reservation can be made");
-
-    const reservedSeats = this.reservationRepo.getReservationQuantity(
-      reservation.Date
-    );
-
-    if (reservedSeats + reservation.Quantity <= this.capacity) {
-      return true;
+    constructor(
+        capacity: number,
+        reservationRepo: IReservationRepository,
+        logger: ILogger
+    ) {
+        this.capacity = capacity;
+        this.reservationRepo = reservationRepo;
+        this.logger = logger;
     }
-    return false;
-  }
+
+    getTotalCapacity(): number {
+        return this.capacity;
+    }
+
+    canReserve(reservation: Reservation): boolean {
+        this.logger.Log("Checking if the reservation can be made");
+
+        const reservedSeats = this.reservationRepo.getReservationQuantity(
+            reservation.Date
+        );
+
+        if (reservedSeats + reservation.Quantity <= this.capacity) {
+            return true;
+        }
+        return false;
+    }
 }
